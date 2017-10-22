d6006fdec3afffa1d288292e4ae7cb3ad7b49b66
Anthony-g2 <ant.greenwell2@gmail.com>
2017-10-22 11:26:08 -0700

    Alert Bar created

---
+++ src/components/ScrollText.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ScrollText extends Component {
+  render(){
+    return (
+      <div className="statusBar">
+        <p className="alert">ALERT!</p>
+      </div>
+    )
+  }
+}
+
+export default ScrollText
